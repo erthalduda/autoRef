ecb7d03b2989d5e2bbedf45c819c66decea99806
erthalduda <eduardaerthal.ssnfq@academico.ifsul.edu.br>
2022-11-18 08:59:35 -0300

    Ola

---
+++ src/main/java/autoref/tcc/autoref/AutorefApplication.java
@@ -0,0 +1,13 @@
+package autoref.tcc.autoref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutorefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutorefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/autoref/tcc/autoref/AutorefApplicationTests.java
@@ -0,0 +1,13 @@
+package autoref.tcc.autoref;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutorefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
