d1bb5f3143865403164785e251539937cf6ff6df
erthalduda <eduardaerthal.ssnfq@academico.ifsul.edu.br>
2022-11-29 09:08:16 -0300

    Criando os controllers do AutoRef

---
+++ src/main/java/autoref/tcc/autoref/controllers/ReferenciaController.java
@@ -0,0 +1,10 @@
+package autoref.tcc.autoref.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/referencias")
+public class ReferenciaController {
+    
+}
