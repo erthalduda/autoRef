8b65c4c1745a70ab4f26dca11f56d2a820a48175
erthalduda <eduardaerthal.ssnfq@academico.ifsul.edu.br>
2022-12-13 09:38:22 -0300

    Criação do DTO de usuario

---
+++ src/main/java/autoref/tcc/autoref/api/dtos/UsuarioDTO.java
@@ -0,0 +1,43 @@
+package autoref.tcc.autoref.api.dtos;
+
+public class UsuarioDTO {
+
+    private String email;
+    private String nome;
+    private String senha;
+
+    public UsuarioDTO(){
+
+    }
+
+    public UsuarioDTO(String email, String nome, String senha) {
+        this.email = email;
+        this.nome = nome;
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return this.senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+}
