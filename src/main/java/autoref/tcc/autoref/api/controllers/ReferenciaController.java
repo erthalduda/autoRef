7a016f76e0602a560b2dd668ff1095e6233d5cbf
erthalduda <eduardaerthal.ssnfq@academico.ifsul.edu.br>
2022-12-20 08:01:37 -0300

    Implementações no controller de referências

--- src/main/java/autoref/tcc/autoref/api/controllers/ReferenciaController.java
+++ src/main/java/autoref/tcc/autoref/api/controllers/ReferenciaController.java
@@ -1,24 +1,56 @@
 package autoref.tcc.autoref.api.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import autoref.tcc.autoref.model.Referencia;
-import autoref.tcc.autoref.repositories.ReferenciaRepository;
+import autoref.tcc.autoref.services.ReferenciaService;
 
 @RestController
 @RequestMapping("/referencias")
 public class ReferenciaController {
 
-    @Autowired
-    private ReferenciaRepository repositorioReferencias;
+    private ReferenciaService serviceReferencia;
+    ModelMapper mapper = new ModelMapper();
+
+    public ReferenciaController(ReferenciaService serviceReferencia) {
+        this.serviceReferencia = serviceReferencia;
+    }
 
     @PostMapping("/cadastrar")
-    public String cadastraReferencia(@RequestBody Referencia referenciaJSON) {
-        repositorioReferencias.save(referenciaJSON);
-        return "Referência cadastrada com sucesso!";
+    public ResponseEntity<?> cadastraReferencia(@RequestBody Referencia referenciaJSON) {
+        try {
+            serviceReferencia.cadastraReferencia(referenciaJSON);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/editar")
+    public ResponseEntity<?> atualizaReferencia(@RequestBody Referencia referenciaJSON) {
+        try {
+            serviceReferencia.atualizaReferencia(referenciaJSON);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/excluir")
+    public ResponseEntity<?> deletaReferencia(@RequestBody Referencia referenciaJSON) {
+        try {
+            serviceReferencia.deletaReferencia(referenciaJSON);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 }
