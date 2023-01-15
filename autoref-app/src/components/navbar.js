f9f238a898229162ed254d0c143906c500138830
William Eduardo Holz <williamholz.ssnfq@academico.ifsul.edu.br>
2023-01-15 18:43:57 +0000

    navbar.js

---
+++ autoref-app/src/components/navbar.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { BsJustify } from "react-icons/bs";
+
+
+import NavbarItem from "./navbaritem";
+
+
+function Navbar() {
+
+
+
+    return (
+
+         
+            <div className="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
+                <div className="container">
+                <BsJustify />
+                
+                    <a href="https://bootswatch.com/" className="navbar-brand" >Autoref</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarResponsive">
+                        <ul className="navbar-nav">
+                            <NavbarItem href="#/home" label="Home" />
+                            <NavbarItem href="#/cadastro" label="Cadastro" />
+                            <NavbarItem href="#/login" label="Login" />
+
+
+
+
+                        </ul>
+                        
+               
+
+
+                    </div>
+                </div>
+            
+            </div>
+            
+         
+            
+
+
+    )
+
+}
+
+export default Navbar
