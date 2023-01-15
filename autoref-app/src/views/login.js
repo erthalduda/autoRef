db3dacd28887df7c264864e3a734cd6785477267
William Eduardo Holz <williamholz.ssnfq@academico.ifsul.edu.br>
2023-01-15 18:39:53 +0000

    Replace login.js

--- autoref-app/src/views/login.js
+++ autoref-app/src/views/login.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Card from "../components/card";
 import FormGroup from "../components/form-group";
+import axios from 'axios'
+import { withRouter } from "react-router-dom";
 
 class Login extends React.Component {
 
@@ -10,60 +12,73 @@ class Login extends React.Component {
     }
 
     entrar = () => {
-        console.log('Email: ', this.state.email)
-        console.log('Senha: ', this.state.senha)
+        axios.post('http://localhost:8080/api/usuarios/autenticar', {
+            email: this.state.email,
+            senha: this.state.senha
+
+        }) .then( response =>{
+            console.log(response)
+        }) .catch( erro => {
+            console.log(erro.response)
+        })
+    }
+    prepareCadastrar = () => {
+
+        this.props.history.push('/cadastro')
+
+
     }
 
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6" style={{ position: 'relative', left: '300px' }}>
-                        <Card tittle="Login">
 
-                            <div className="row">
-                                <div className="col-lg-12">
-                                    <div className="bs-component">
-                                        <fieldset>
-                                            <FormGroup label='E-mail ' htmlFor="exampleInputEmail1">
-                                                <input type='email'
-                                                    value={this.state.email}
-                                                    onChange={e => this.setState({ email: e.target.value })}
-                                                    className="form-control"
-                                                    id="exampleInputEmail1"
-                                                    aria-describedby="emailHelp" placeholder="Digite o email" />
+            <div className="row">
+                <div className="col-md-6" style={{ position: 'relative', left: '300px' }}>
+                    <Card tittle="Login">
 
-
-                                            </FormGroup>
-
-                                            <FormGroup label='Senha ' htmlFor="exampleInputPassword1">
-                                                <input type='Password'
-                                                    value={this.state.senha}
-                                                    onChange={e => this.setState({ senha: e.target.value })}
-                                                    className="form-control"
-                                                    id="exampleInputPassword1" placeholder="Password" />
-
-                                            </FormGroup>
-                                           
-                                            <button onClick={this.entrar} className="btn btn-success">Entrar</button>
-                                            <button className="btn btn-danger">Cadastrar</button>
-
-                                        </fieldset>
+                        <div className="row">
+                            <div className="col-lg-12">
+                                <div className="bs-component">
+                                    <fieldset>
+                                        <FormGroup label='E-mail ' htmlFor="exampleInputEmail1">
+                                            <input type='email'
+                                                value={this.state.email}
+                                                onChange={e => this.setState({ email: e.target.value })}
+                                                className="form-control"
+                                                id="exampleInputEmail1"
+                                                aria-describedby="emailHelp" placeholder="Digite o email" />
 
 
-                                    </div>
+                                        </FormGroup>
+
+                                        <FormGroup label='Senha ' htmlFor="exampleInputPassword1">
+                                            <input type='Password'
+                                                value={this.state.senha}
+                                                onChange={e => this.setState({ senha: e.target.value })}
+                                                className="form-control"
+                                                id="exampleInputPassword1" placeholder="Password" />
+
+                                        </FormGroup>
+
+                                        <button onClick={this.entrar} className="btn btn-success">Entrar</button>
+                                        <button onClick={this.prepareCadastrar} className="btn btn-danger">Cadastrar</button>
+
+                                    </fieldset>
+
+
                                 </div>
                             </div>
+                        </div>
 
-                        </Card>
+                    </Card>
 
 
 
 
-                    </div>
                 </div>
-
             </div>
+
+
         )
     }
 
@@ -71,4 +86,4 @@ class Login extends React.Component {
 
 }
 
-export default Login
+export default withRouter(Login)
