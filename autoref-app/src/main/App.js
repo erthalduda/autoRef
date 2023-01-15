4c4298f10bd726e63c4d614b7a7d3bd191a83a7c
William Eduardo Holz <williamholz.ssnfq@academico.ifsul.edu.br>
2023-01-15 18:48:36 +0000

    app.js

---
+++ autoref-app/src/main/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import Rotas from './rotas';
+
+import Navbar from '../components/navbar';
+
+
+
+import 'bootswatch/dist/minty/bootstrap.css'
+import '../custom.css'
+
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <>
+ 
+        <Navbar />
+        
+        
+        
+       
+        <div className="container">
+
+          <Rotas />
+        </div>
+      </>
+    )
+  }
+}
+
+export default App;
