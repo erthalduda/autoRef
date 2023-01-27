d9eead0673fa980955e13db1064935bc6aafc56c
erthalduda <eduardaerthal.ssnfq@academico.ifsul.edu.br>
2023-01-27 09:00:57 -0300

    Criação do map de tipos de referência

--- src/main/java/autoref/tcc/autoref/api/controllers/ReferenciaController.java
+++ src/main/java/autoref/tcc/autoref/api/controllers/ReferenciaController.java
@@ -1,5 +1,8 @@
 package autoref.tcc.autoref.api.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,33 +13,45 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import autoref.tcc.autoref.api.dtos.ReferenciaDTO;
-import autoref.tcc.autoref.api.dtos.UsuarioDTO;
 import autoref.tcc.autoref.exceptions.ExcecoesAutoref;
-import autoref.tcc.autoref.model.Referencia;
-import autoref.tcc.autoref.model.Usuario;
+import autoref.tcc.autoref.model.*;
 import autoref.tcc.autoref.services.ReferenciaService;
 
 @RestController
 @RequestMapping("/referencias")
 public class ReferenciaController {
 
+    private Map<String, Class<? extends Referencia>> tipos = new HashMap<>();
+
     private ReferenciaService serviceReferencia;
     ModelMapper mapper = new ModelMapper();
 
     public ReferenciaController(ReferenciaService serviceReferencia) {
         this.serviceReferencia = serviceReferencia;
+
+        tipos.put("artigo", Artigo.class);
+        tipos.put("monografia", Monografia.class);
+        tipos.put("parteMonografia", ParteMonografia.class);
+        tipos.put("monografiaMeioEletronico", MonografiaMeioEletronico.class);
+        tipos.put("monografiaOnline", MonografiaOnline.class);
+        tipos.put("colecaoPublicacaoPeriodica", ColecaoPublicacaoPeriodica.class);
+        tipos.put("parteColecaoPublicacaoPeriodica", ParteColecaoPublicacaoPeriodica.class);
+        tipos.put("colecaoPublicacaoPeriodicaEletronico", ColecaoPublicacaoPeriodicaEletronico.class);
+        tipos.put("fasciculo", Fasciculo.class);
+        tipos.put("trabalhoAcademico", TrabalhoAcademico.class);
     }
 
     @PostMapping("/cadastrar")
-    public ResponseEntity<?> cadastraReferencia(@RequestBody ReferenciaDTO referenciaDTO, UsuarioDTO usuarioDTO) {
-        Referencia referencia = mapper.map(referenciaDTO, Referencia.class);
-        Usuario usuario = mapper.map(usuarioDTO, Usuario.class);
+    public ResponseEntity<?> cadastraReferencia(@RequestBody ReferenciaDTO referenciaDTO) {
+
+        Referencia referencia = mapper.map(referenciaDTO, tipos.get(referenciaDTO.getTipo()));
+
+        // Usuario usuario = mapper.map(usuarioDTO, Usuario.class);
         try {
-            Referencia salva = serviceReferencia.cadastraReferencia(referencia, usuario);
+            Referencia salva = serviceReferencia.cadastraReferencia(referencia);
             return new ResponseEntity<>(salva, HttpStatus.CREATED);
         } catch (ExcecoesAutoref excecao) {
             return new ResponseEntity<>(excecao.getMessage(), HttpStatus.BAD_REQUEST);
@@ -64,7 +79,7 @@ public class ReferenciaController {
     }
 
     @GetMapping("/buscar/{idReferencia}")
-    public ResponseEntity<?> buscaReferenciaPorId(@PathVariable(name = "idReferencia") Integer idReferencia){
+    public ResponseEntity<?> buscaReferenciaPorId(@PathVariable(name = "idReferencia") Integer idReferencia) {
         try {
             Referencia buscar = serviceReferencia.encontrarPorId(idReferencia);
             return new ResponseEntity<>(buscar, HttpStatus.OK);
