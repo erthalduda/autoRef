f6345f70619ab31ae55c6f23b070fce549e9c7a4
erthalduda <eduardaerthal.ssnfq@academico.ifsul.edu.br>
2023-01-29 16:49:20 -0300

    Formatação de monografia em meio eletrônico

--- src/main/java/autoref/tcc/autoref/model/MonografiaMeioEletronico.java
+++ src/main/java/autoref/tcc/autoref/model/MonografiaMeioEletronico.java
@@ -31,4 +31,22 @@ public class MonografiaMeioEletronico extends Monografia {
 		this.descricaoDoSuporte = descricaoDoSuporte;
 	}
 
+	@Override
+	public void formata() {
+		String autores = this.formataAutores();
+		String titulo = this.getTitulo().toUpperCase();
+		String subtitulo = "";
+		if (!this.getSubtitulo().equals(" ")) {
+			subtitulo = ": " + this.getSubtitulo() + ". ";
+		} else {
+			subtitulo = ". ";
+		}
+		String edicao = this.getEdicao() + ". ed. ";
+		String local = this.getCidade() + ": " + this.getEditora() + ", " + this.getAnoPublicacao() + ". ";
+		String paginas = this.getQuantidadePaginas() + "p.";
+		String descricaoSuporte = this.getDescricaoDoSuporte() + ".";
+		String formatoFinal = autores + titulo + subtitulo + edicao + local + paginas + descricaoSuporte;
+		this.formatoFinal = formatoFinal;
+	}
+
 }
