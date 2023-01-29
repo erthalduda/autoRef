708b975f0d0fbe6be59ff40c406929dc94d7d9f3
erthalduda <eduardaerthal.ssnfq@academico.ifsul.edu.br>
2023-01-29 16:48:31 -0300

    Formatação de monografia online

--- src/main/java/autoref/tcc/autoref/model/MonografiaOnline.java
+++ src/main/java/autoref/tcc/autoref/model/MonografiaOnline.java
@@ -1,6 +1,8 @@
 package autoref.tcc.autoref.model;
 
+import java.sql.Date;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -10,14 +12,12 @@ public class MonografiaOnline extends Monografia {
 
 	private String url;
 
-	private String dataAcesso;
+	private Date dataAcesso;
 
 	public MonografiaOnline(Integer id, String titulo, String formatoFinal, String citacaoIndireta,
 			String citacaoDireta, String tipo, List<Colecao> colecoes, Usuario usuario, ArrayList<String> autor,
-			String subtitulo, String anoPublicacao, String cidade, String anoEntrega,
-			String quantidadePaginas, String edicao, String editora, String url, String dataAcesso) {
-		super(id, titulo, formatoFinal, citacaoIndireta, citacaoDireta, tipo, colecoes, usuario, autor, subtitulo,
-				anoPublicacao, cidade, anoEntrega, quantidadePaginas, edicao, editora);
+			String subtitulo, String anoPublicacao, String cidade, String anoEntrega, String edicao, String editora, String url, Date dataAcesso) {
+		super();
 		this.url = url;
 		this.dataAcesso = dataAcesso;
 	}
@@ -34,12 +34,76 @@ public class MonografiaOnline extends Monografia {
 		this.url = url;
 	}
 
-	public String getDataAcesso() {
+	public Date getDataAcesso() {
 		return this.dataAcesso;
 	}
 
-	public void setDataAcesso(String dataAcesso) {
+	public void setDataAcesso(Date dataAcesso) {
 		this.dataAcesso = dataAcesso;
+	}
+
+	@Override
+	public void formata() {
+
+		Calendar calendario = Calendar.getInstance();
+		calendario.setTime(this.getDataAcesso());
+		int mes = calendario.get(Calendar.MONTH);
+		int dia = calendario.get(Calendar.DAY_OF_MONTH);
+		int ano = calendario.get(Calendar.YEAR);
+
+		String autores = this.formataAutores();
+		String titulo = this.getTitulo().toUpperCase();
+		String subtitulo = "";
+		if (this.getSubtitulo()!=null) {
+			subtitulo = ": " + this.getSubtitulo() + ". ";
+		} else {
+			subtitulo = ". ";
+		}
+		String edicao = this.getEdicao() + ". ed. ";
+		String local = this.getCidade() + ": " + this.getEditora() + ", " + this.getAnoPublicacao() + ". ";
+		String mesString = "";
+		switch (mes) {
+			case 1:
+				mesString = " jan. ";
+				break;
+			case 2:
+				mesString = " fev. ";
+				break;
+			case 3:
+				mesString = " mar. ";
+				break;
+			case 4:
+				mesString = " abr. ";
+				break;
+			case 5:
+				mesString = " maio ";
+				break;
+			case 6:
+				mesString = " jun. ";
+				break;
+			case 7:
+				mesString = " jul. ";
+				break;
+			case 8:
+				mesString = " ago. ";
+				break;
+			case 9:
+				mesString = " set. ";
+				break;
+			case 10:
+				mesString = " out. ";
+				break;
+			case 11:
+				mesString = " nov. ";
+				break;
+			case 12:
+				mesString = " dez. ";
+				break;
+		}
+		String url = ". Disponível em: " + this.getUrl() + ". Acesso em: " + dia + mesString + ano + ".";
+		String formatoFinal = autores + titulo + subtitulo + edicao + local + url;
+		this.formatoFinal = formatoFinal;
+
 	}
 
 }
