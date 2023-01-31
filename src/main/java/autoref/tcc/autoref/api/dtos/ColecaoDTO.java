9959012fb60700f96b3829c97e2e38a289621347
erthalduda <eduardaerthal.ssnfq@academico.ifsul.edu.br>
2023-01-31 09:36:07 -0300

    Criação da coleçãoDTO

--- src/main/java/autoref/tcc/autoref/api/dtos/ColecaoDTO.java
+++ src/main/java/autoref/tcc/autoref/api/dtos/ColecaoDTO.java
@@ -1,5 +1,24 @@
 package autoref.tcc.autoref.api.dtos;
 
 public class ColecaoDTO {
+
+    String nome;
+
+    public ColecaoDTO() {
+    }
+
+    public ColecaoDTO(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    };
+
+
     
 }
