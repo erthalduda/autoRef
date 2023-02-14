091687e49b8bbcbce53548f3601a7223f7c7ec75
erthalduda <eduardaerthal.ssnfq@academico.ifsul.edu.br>
2023-02-14 07:53:36 -0300

    Limpeza de código

--- src/main/java/autoref/tcc/autoref/AutorefApplication.java
+++ src/main/java/autoref/tcc/autoref/AutorefApplication.java
@@ -1,7 +1,5 @@
 package autoref.tcc.autoref;
 
-// import java.util.ArrayList;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
