35b183e8ba5e4aad59aabe5ab295584840f0e725
erthalduda <eduardaerthal.ssnfq@academico.ifsul.edu.br>
2023-02-20 21:52:07 -0300

    Teste de repositório geral

--- autoref-app/src/main/rotas.js
+++ autoref-app/src/main/rotas.js
@@ -2,12 +2,11 @@ import React from "react";
 
 import Login from "../views/login";
 import Login2 from "../views/login02";
-import Home from '../views/home';
+import Home from "../views/home";
 import CadastroUsuario from "../views/cadastro-usuario";
 import Referencias from "../views/referencias";
 
-
-import { Route, Switch, HashRouter } from 'react-router-dom'
+import { Route, Switch, HashRouter } from "react-router-dom";
 import Monografia from "../views/monografia";
 import Website from "../views/website";
 import Artigo from "../views/artigo";
@@ -23,37 +22,41 @@ import PartePeriodica from "../views/parte-periodica";
 import ColecaoPeriodicaMeioEletronico from "../views/colecao-periodica-meio-eletronico";
 import Faq from "../views/faq";
 import CadastroColecao from "../views/cadastro-colecao";
+import RepositorioGeral from "../views/repositorio-geral";
+import RepositorioPrivado from "../views/repositorio-privado";
 //import Login2 from "../views/login02";
 
-
 function Rotas() {
-    return (
-        <HashRouter>
-            <Switch>
-                <Route path="/login" component={Login} />
-                <Route path="/login2" component={Login2} />
-                <Route path="/home" component={Home} />
-                <Route path="/cadastro" component={CadastroUsuario} />
-                <Route path="/referencias" component={Referencias} />
-                <Route path="/faq" component={Faq} />
-                <Route path="/monografia" component={Monografia} />
-                <Route path="/website" component={Website} />
-                <Route path="/artigo" component={Artigo} />
-                <Route path="/trabalho" component={TrabalhoAcademico} />
-                <Route path="/periodico" component={Periodico} />
-                <Route path="/opcoes_mono" component={MonoOpcoes} />
-                <Route path="/parte_monografia" component={ParteMonografia} />
-                <Route path="/monografia_online" component={MonografiaOnline} />
-                <Route path="/monografia_eletronica" component={MonografiaEletronica} />
-                <Route path="/colecao_periodica" component={ColecaoPeriodica} />
-                <Route path="/parte_periodica" component={PartePeriodica} />
-                <Route path="/colecao_periodica_eletronica" component={ColecaoPeriodicaMeioEletronico} />
-                <Route path="/cadastro_colecao" component={CadastroColecao}/>
-                
-
-            </Switch>
-        </HashRouter>
-    )
+  return (
+    <HashRouter>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/login2" component={Login2} />
+        <Route path="/home" component={Home} />
+        <Route path="/cadastro" component={CadastroUsuario} />
+        <Route path="/referencias" component={Referencias} />
+        <Route path="/faq" component={Faq} />
+        <Route path="/monografia" component={Monografia} />
+        <Route path="/website" component={Website} />
+        <Route path="/artigo" component={Artigo} />
+        <Route path="/trabalho" component={TrabalhoAcademico} />
+        <Route path="/periodico" component={Periodico} />
+        <Route path="/opcoes_mono" component={MonoOpcoes} />
+        <Route path="/parte_monografia" component={ParteMonografia} />
+        <Route path="/monografia_online" component={MonografiaOnline} />
+        <Route path="/monografia_eletronica" component={MonografiaEletronica} />
+        <Route path="/colecao_periodica" component={ColecaoPeriodica} />
+        <Route path="/parte_periodica" component={PartePeriodica} />
+        <Route
+          path="/colecao_periodica_eletronica"
+          component={ColecaoPeriodicaMeioEletronico}
+        />
+        <Route path="/cadastro_colecao" component={CadastroColecao} />
+        <Route path="/repositorio_geral" component={RepositorioGeral} />
+        <Route path="/repositorio_privado" component={RepositorioPrivado} />
+      </Switch>
+    </HashRouter>
+  );
 }
 
-export default Rotas
+export default Rotas;
---
+++ autoref-app/src/views/repositorio-geral.jsx
@@ -0,0 +1,145 @@
+import React from "react";
+//import FormGroup from "../components/form-group";
+
+import { useState } from "react";
+import axios from "axios";
+//import login from "./login";
+
+import "../css/referencia.css";
+
+function RepositorioGeral() {
+  const [inputFields, setInputFields] = useState([
+    {
+      id: 1,
+      name: "pesquisa",
+      value: "",
+      type: "text",
+    },
+  ]);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    axios.post("https://localhost/3000/referencias/cadastrar", inputFields);
+    console.log(inputFields);
+  };
+
+  const handleFormChange = (id, event) => {
+    let data = [...inputFields];
+    const inputFiltrado = data.find((input) => input.id === id);
+
+    inputFiltrado.value = event.target.value;
+
+    data = data.map((inputField) => {
+      if (inputField.id === inputFiltrado.id) {
+        inputField = { ...inputFiltrado };
+      }
+      return inputField;
+    });
+    // //data[index][event.target.name] = event.target.value;
+    setInputFields(data);
+  };
+
+  return (
+    <>
+      <nav
+        id="sidebarMenu"
+        className="collapse d-lg-block sidebar collapse bg-white"
+      >
+        <div className="position-sticky">
+          <div className="list-group list-group-flush mx-3 mt-4">
+            <a
+              href="#/referencias"
+              className="list-group-item list-group-item-action py-2 ripple"
+              aria-current="true"
+            >
+              <i className="fas fa-tachometer-alt fa-fw me-3"></i>
+              <span>REFERÊNCIAS</span>
+            </a>
+
+            <a
+              href="#"
+              className="list-group-item list-group-item-action py-2 ripple"
+              aria-current="true"
+            >
+              <i className="fas fa-tachometer-alt fa-fw me-3"></i>
+              <span>BUSCAR REFERÊNCIAS</span>
+            </a>
+            <a
+              href="#"
+              className="list-group-item list-group-item-action py-2 ripple"
+              aria-current="true"
+            >
+              <i className="fas fa-tachometer-alt fa-fw me-3"></i>
+              <span>COLEÇÕES</span>
+            </a>
+            {/* <a
+              href="#"
+              className="list-group-item list-group-item-action py-2 ripple"
+              aria-current="true"
+            >
+              <i className="fas fa-tachometer-alt fa-fw me-3"></i>
+              <span>CONQUISTAS</span>
+            </a> */}
+            <a
+              href="#"
+              className="list-group-item list-group-item-action py-2 ripple"
+              aria-current="true"
+            >
+              <i className="fas fa-tachometer-alt fa-fw me-3"></i>
+              <span>RANKING</span>
+            </a>
+            <a
+              href="#"
+              className="list-group-item list-group-item-action py-2 ripple"
+              aria-current="true"
+            >
+              <i className="fas fa-tachometer-alt fa-fw me-3"></i>
+              <span>FAQ</span>
+            </a>
+            <a
+              href="#"
+              className="list-group-item list-group-item-action py-2 ripple"
+              aria-current="true"
+            >
+              <i className="fas fa-tachometer-alt fa-fw me-3"></i>
+              <span>EDITAR PERFIL</span>
+            </a>
+          </div>
+        </div>
+      </nav>
+      <h1 id="referencia">Repositório Geral</h1>
+      <div>
+        <div className="form-group-ref">
+          <form onSubmit={onSubmit}>
+            {inputFields.map((input, index) => {
+              return (
+                <div key={index}>
+                  <input
+                    type={input.type}
+                    key={input.id}
+                    id={input.id}
+                    value={input.value}
+                    name={input.name}
+                    placeholder={input.placeholder}
+                    className="form-input"
+                    onChange={(e) => handleFormChange(input.id, e)}
+                  />
+                </div>
+              );
+            })}
+
+            <div>
+              <button type="submit" className="btn btn-success">
+                Pesquisar
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      {/* <button onClick={this.entrar} className="btn btn-success">Adicionar</button> */}
+    </>
+  );
+}
+
+export default RepositorioGeral;
