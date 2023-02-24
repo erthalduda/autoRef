180b4acf4edeeb5e2aec8597a1b7bc53e48f32e5
erthalduda <eduardaerthal.ssnfq@academico.ifsul.edu.br>
2023-02-24 19:42:59 -0300

    Botão de logout

--- autoref-app/src/components/navbar.jsx
+++ autoref-app/src/components/navbar.jsx
@@ -1,35 +1,26 @@
 import React, { useRef, useState } from "react";
 
-import Profile from "../images/profile.png";
+import { BsBoxArrowRight } from "react-icons/bs";
 
 function Navbar() {
-  const dropDownRef = useRef(null);
   const [isActive, setIsActive] = useState(false);
   const onClick = () => setIsActive(!isActive);
 
   return (
     <div className="navbar fixed-top">
       <div className="container">
-        <a href="#/home" className="navbar-brand-dois">
+
+        <a href="/home" className="navbar-brand-dois">
           <b>AUTOREF</b>
         </a>
 
         <div className="menu-profile">
-          <button onClick={onClick} className="menu-button">
-            <img src={Profile} alt="Avatar" height="20"></img>{" "}
-            <b className="caret"></b>
-          </button>
-
-          <nav
-            ref={dropDownRef}
-            className={`menu ${isActive ? "active" : "inactive"}`}
-          >
-            <ul>
-              <li className="log">
-                <a href="#/home">Logout</a>
-              </li>
-            </ul>
-          </nav>
+          <div className="container">
+            <a href="/" className="navbar-brand-dois">
+              <b>Logout </b>
+              <BsBoxArrowRight></BsBoxArrowRight>
+            </a>
+          </div>
         </div>
       </div>
     </div>
