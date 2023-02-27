8b49052280e824b82ab12cae0e4afd422d0b3360
erthalduda <eduardaerthal.ssnfq@academico.ifsul.edu.br>
2023-02-27 17:08:35 -0300

    Mockup ranking enquanto nao funciona

--- autoref-app/src/views/ranking.jsx
+++ autoref-app/src/views/ranking.jsx
@@ -1,20 +1,37 @@
 import React, { useEffect, useState } from "react";
 import "../css/referencia.css";
 import axios from "axios";
+import UsuarioService from "../app/services/UsuarioService";
 import Sidebar from "../components/sidebar";
 import Navbar from "../components/navbar";
+import * as messages from "../components/toastifyClasse";
 
 const Ranking = () => {
-  const [ranking, setRanking] = useState([]);
+  // this.service = new UsuarioService();
 
-  useEffect(() => {
-    dadosRanking();
-  }, []);
+  // this.service.rankingUsuarios()
+  //   .then((resposta) => {
+  //     const lista = resposta.data;
 
-  const dadosRanking = async () => {
-    const { data } = await axios.get("http://localhost:8080/usuarios/ranking");
-    setRanking(data);
-  };
+  //     if (lista.length < 1) {
+  //       messages.mensagemAlerta("Nenhuma referência foi encontrada.");
+  //     }
+  //     this.setState({ referencias: lista });
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
+
+  // const [ranking, setRanking] = useState([]);
+
+  // useEffect(() => {
+  //   dadosRanking();
+  // }, []);
+
+  // const dadosRanking = async () => {
+  //   const { data } = await axios.get("http://localhost:8080/usuarios/ranking");
+  //   setRanking(data);
+  // };
 
   return (
     <>
@@ -22,14 +39,37 @@ const Ranking = () => {
       <Navbar></Navbar>
       <div className="jumbotron">
         <h1 className="centralizar-ranking">RANKING DE USUÁRIOS</h1>
-
-        <div className="centralizar"><h1>A</h1>
-          {ranking.map((rank) => (
-            <div key={rank}>
-              <h5>{rank}</h5>
-            </div>
-          ))}
-        </div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">NOME</th>
+              <th scope="col">XP</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td> RODRIGO REMOR </td>
+              <td>4500XP</td>
+            </tr>{" "}
+            <tr>
+              <td>2</td>
+              <td> LOURENÇO BASSO </td>
+              <td>3200XP</td>
+            </tr>{" "}
+            <tr>
+              <td>3</td>
+              <td> WILLIAM HOLZ </td>
+              <td> 3000XP</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td> EDUARDA ERTHAL </td>
+              <td> 2800XP</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </>
   );
