447e28417742c82e94fd8e31160cf1ce1723ba52
erthalduda <eduardaerthal.ssnfq@academico.ifsul.edu.br>
2023-02-27 10:59:32 -0300

    Centralizando opções

--- autoref-app/src/views/referencias.jsx
+++ autoref-app/src/views/referencias.jsx
@@ -33,27 +33,36 @@ const Referencias = () => {
           style={{ position: "relative", left: "300px" }}
         >
           <div className="jumbotron">
-            <h1 className="display-3">CADASTRAR REFERÊNCIA</h1>
-            <p className="lead">
+            <h1 className="centralizar-nome">CADASTRAR</h1>
+            <h1 className="centralizar-nome">REFERÊNCIA</h1>
+            <p className="centralizar">
               Escolha o tipo de referência que deseja cadastrar:
             </p>
-            <div className="button1">
+            <div className="centralizar">
               <button onClick={prepareMonografia} className="btn btn-success">
                 Monografia
               </button>
+            </div>
+            <div className="centralizar">
               <button onClick={prepareArtigo} className="btn btn-success">
                 Artigo
               </button>
+            </div>
+            <div className="centralizar">
               <button onClick={prepareTrabalho} className="btn btn-success">
                 Trabalho Acadêmico
               </button>
+            </div>
+            <div className="centralizar">
               <button onClick={preparePeriodico} className="btn btn-success">
                 Publicação Periódica
               </button>
-              <button onClick={prepareWebsite} className="btn btn-success">
-                Website
-              </button>
             </div>
+              <div className="centralizar">
+                <button onClick={prepareWebsite} className="btn btn-success">
+                  Website
+                </button>
+              </div>
           </div>
         </div>
       </div>
