a1c07dc38312f77ab3ffcdc09bf2c25f1a479aa5
erthalduda <eduardaerthal.ssnfq@academico.ifsul.edu.br>
2023-02-27 09:37:18 -0300

    Coleções usuário

--- autoref-app/src/views/colecoes-usuario.jsx
+++ autoref-app/src/views/colecoes-usuario.jsx
@@ -1,96 +1,93 @@
-// import React from "react";
-// import { useState } from "react";
-// import { useHistory } from "react-router-dom";
-// import axios from "axios";
-// import Navbar from "../components/navbar";
-// import "../css/referencia.css";
-// import Sidebar from "../components/sidebar";
+import React from "react";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import "../css/referencia.css";
+import Sidebar from "../components/sidebar";
+import Navbar from "../components/navbar";
 
-// function ColecoesUsuario() {
-//   const [colecoes, setColecoes] = useState([]);
-//   const [loading, setLoading] = useState(false);
-//   const [errorMessage, setError] = useState(false);
+function ColecoesUsuario() {
+  const [inputFields, setInputFields] = useState([]);
+  let history = useHistory();
+  
+  const prepareCadastrarColecao = () => {
+    history.push("/cadastro_colecao");
+  };
 
-//   let history = useHistory();
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    axios.post("https://localhost/8080/colecoes/cadastrar", inputFields);
+    console.log(inputFields);
+  };
 
-//   const prepareCadastrarColecao = () => {
-//     history.push("/cadastro_colecao");
-//   };
+  const handleFormChange = (id, event) => {
+    let data = [...inputFields];
+    const inputFiltrado = data.find((input) => input.id === id);
 
-//   const onSubmit = async (event) => {
-//     event.preventDefault();
-//     axios.post("https://localhost/8080/colecoes/cadastrar", inputFields);
-//     console.log(inputFields);
-//   };
+    inputFiltrado.value = event.target.value;
 
-//   const handleFormChange = (id, event) => {
-//     let data = [...inputFields];
-//     const inputFiltrado = data.find((input) => input.id === id);
+    data = data.map((inputField) => {
+      if (inputField.id === inputFiltrado.id) {
+        inputField = { ...inputFiltrado };
+      }
+      return inputField;
+    });
+    // //data[index][event.target.name] = event.target.value;
+    setInputFields(data);
+  };
 
-//     inputFiltrado.value = event.target.value;
+  // const onChangeAutor = (value, autor) => {
 
-//     data = data.map((inputField) => {
-//       if (inputField.id === inputFiltrado.id) {
-//         inputField = { ...inputFiltrado };
-//       }
-//       return inputField;
-//     });
-//     // //data[index][event.target.name] = event.target.value;
-//     setInputFields(data);
-//   };
+  //   const valor = value.target.value;
+  // let autorFiltrado = inputFields.find(a => {
+  //   return autor?.id === a?.id
+  // })
 
-//   // const onChangeAutor = (value, autor) => {
+  //     console.log(autorFiltrado)
+  //     autorFiltrado.autor = valor;
 
-//   //   const valor = value.target.value;
-//   // let autorFiltrado = inputFields.find(a => {
-//   //   return autor?.id === a?.id
-//   // })
+  //     console.log(inputFields)
+  //     //this.setState({ autor: autor.target.value })
+  // }
 
-//   //     console.log(autorFiltrado)
-//   //     autorFiltrado.autor = valor;
+  return (
+    <>
+      <Sidebar></Sidebar>
+      <Navbar></Navbar>
+      <h1 id="referencia">Suas coleções</h1>
+      <br></br>
+      <div className="form-group-ref">
+        <form onSubmit={onSubmit}>
+          {inputFields.map((input, index) => {
+            return (
+              <div key={index}>
+                <input
+                  type={input.type}
+                  key={input.id}
+                  id={input.id}
+                  value={input.value}
+                  name={input.name}
+                  placeholder={input.placeholder}
+                  className="form-input"
+                  onChange={(e) => handleFormChange(input.id, e)}
+                />
+              </div>
+            );
+          })}
 
-//   //     console.log(inputFields)
-//   //     //this.setState({ autor: autor.target.value })
-//   // }
+          <div className="sla">
+            <button
+              type="submit"
+              className="btn btn-primary"
+              onClick={prepareCadastrarColecao}
+            >
+              +
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
 
-//   return (
-//     <>
-//       <Sidebar></Sidebar>
-//       <Navbar></Navbar>
-//       <h1 id="referencia">Suas coleções</h1>
-//       <br></br>
-//       <div className="form-group-ref">
-//         <form onSubmit={onSubmit}>
-//           {inputFields.map((input, index) => {
-//             return (
-//               <div key={index}>
-//                 <input
-//                   type={input.type}
-//                   key={input.id}
-//                   id={input.id}
-//                   value={input.value}
-//                   name={input.name}
-//                   placeholder={input.placeholder}
-//                   className="form-input"
-//                   onChange={(e) => handleFormChange(input.id, e)}
-//                 />
-//               </div>
-//             );
-//           })}
-
-//           <div className="sla">
-//             <button
-//               type="submit"
-//               className="btn btn-primary"
-//               onClick={prepareCadastrarColecao}
-//             >
-//               +
-//             </button>
-//           </div>
-//         </form>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default ColecoesUsuario;
+export default ColecoesUsuario;
